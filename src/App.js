974f69984b9bf8f5f36a09e6fada97f878c8c3ec
wangke <wannngkee@gmail.com>
2022-04-04 22:27:48 +1000

    adjust style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PriceTable from "./components/PriceTable";
 function App() {
   return (
     <div className="App">
-      <h2 id="title">Cryptocurrency Prices by Market Cap</h2>
+      <h2>Cryptocurrency Prices by Market Cap</h2>
       <PriceTable />
     </div>
   );
